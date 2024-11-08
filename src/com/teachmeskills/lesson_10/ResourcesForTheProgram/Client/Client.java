271808f8a6ea69e6f79a68911ac037aeccaa3563
PC\danii <daniil.minsk.dragun@gmail.com>
2024-11-08 03:41:02 +0300

    Added new packages. Fixed comparison method. Added main class.

---
+++ src/com/teachmeskills/lesson_10/ResourcesForTheProgram/Client/Client.java
@@ -0,0 +1,50 @@
+package com.teachmeskills.lesson_10.ResourcesForTheProgram.Client;
+
+import com.teachmeskills.lesson_10.ResourcesForTheProgram.CreditCard.CreditCard;
+
+import java.util.Scanner;
+
+public class Client {
+
+    private String clientName;
+
+    public CreditCard[] CreatingClientCards() {
+
+        System.out.println("Enter the number of client cards:");
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOfCards = scanner.nextInt();
+
+        CreditCard[] cards = new CreditCard[numberOfCards];
+
+        if (numberOfCards == 0) {
+
+            System.out.println("The client has no cards.");
+
+        } else if (numberOfCards == 1) {
+
+            System.out.println("The client has only 1 card.");
+            System.out.println("Enter cvv code, card number, and amount for card: ");
+
+            cards[0] = new CreditCard(scanner.nextInt(), scanner.nextInt(), scanner.nextDouble());
+
+        } else {
+            for (int i = 0; i < numberOfCards; i++) {
+
+                System.out.println("Enter cvv code, card number, and amount for card " + (i + 1) + ": ");
+
+                cards[i] = new CreditCard(scanner.nextInt(), scanner.nextInt(), scanner.nextDouble());
+            }
+        }
+
+        return cards;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+}
---
+++ src/com/teachmeskills/lesson_10/ResourcesForTheProgram/CreditCard/CreditCard.java
@@ -0,0 +1,72 @@
+package com.teachmeskills.lesson_10.ResourcesForTheProgram.CreditCard;
+
+import java.util.Objects;
+
+public class CreditCard {
+
+    private int cvv;
+    private int cardNumber;
+    private double amountOnTheCard;
+
+    private CreditCard[] cards;
+
+    public CreditCard(int cardNumber, int cvv, double amountOnTheCard) {
+        this.cardNumber = cardNumber;
+        this.cvv = cvv;
+        this.amountOnTheCard = amountOnTheCard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreditCard that = (CreditCard) o;
+        return Double.compare(cvv, that.cvv) == 0 && Double.compare(cardNumber, that.cardNumber) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cvv, cardNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "cardNumber=" + cardNumber +
+                ", cvv=" + cvv +
+                ", amountOnTheCard=" + amountOnTheCard +
+                '}';
+    }
+
+    public double getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public double getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public double getAmountOnTheCard() {
+        return amountOnTheCard;
+    }
+
+    public void setAmountOnTheCard(double amountOnTheCard) {
+        this.amountOnTheCard = amountOnTheCard;
+    }
+
+    public CreditCard[] getCards() {
+        return cards;
+    }
+
+    public void setCards(CreditCard[] cards) {
+        this.cards = cards;
+    }
+}
